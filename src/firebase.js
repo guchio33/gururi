4de00fd8586f901fbc4121acef7096c35809b33c
DaikiOkada <d.okada0820@gmail.com>
2021-07-07 11:01:29 +0900

    yarn serveでエラー起こるのでnpm install --save実行

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,15 @@
-import firebase from "firebase";
-import "firebase/firestore";
+import firebase from 'firebase'
+import 'firebase/firestore'
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDX6Cqy2WGiFRiW0b8gXq3X3pdjDt6C9hE",
-  authDomain: "gururi-db37f.firebaseapp.com",
-  projectId: "gururi-db37f",
-  storageBucket: "gururi-db37f.appspot.com",
-  messagingSenderId: "219413566946",
-  appId: "1:219413566946:web:e6eceb6f1bfef2cdb5d5bb",
-  measurementId: "G-CH5FC28V89",
-};
+  apiKey: 'AIzaSyDX6Cqy2WGiFRiW0b8gXq3X3pdjDt6C9hE',
+  authDomain: 'gururi-db37f.firebaseapp.com',
+  projectId: 'gururi-db37f',
+  storageBucket: 'gururi-db37f.appspot.com',
+  messagingSenderId: '219413566946',
+  appId: '1:219413566946:web:e6eceb6f1bfef2cdb5d5bb',
+  measurementId: 'G-CH5FC28V89',
+}
 
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig)
