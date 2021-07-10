e14cc6205730eb2bfdbd19e509e59907e3fb6d6b
DaikiOkada <d.okada0820@gmail.com>
2021-07-11 02:07:59 +0900

    現在地取得　現在地追跡　OK?

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,15 +1,16 @@
 <template>
   <div style="height: 500px; width: 100%">
-    <button @click="now">現在地</button>
-    <!-- <button @click="kamakura">鎌倉</button> -->
+    <button @click="getCurrentPosition">現在地</button>
+    <button @click="kamakura">鎌倉</button>
     <l-map
-      v-if="showMap"
       :zoom="zoom"
       :center="center"
       :options="mapOptions"
       style="height: 80%"
       @ready="onReady"
       @locationfound="onLocationFound"
+      @update:zoom="zoomUpdated"
+      @update:center="centerUpdated"
     >
       <l-tile-layer
         :url="url"
@@ -30,7 +31,7 @@
           <div>I am a tooltip</div>
         </l-tooltip>
       </l-marker>
-      <l-marker :lat-lng="currentLocation"> </l-marker>
+      <l-marker :lat-lng="currentCenter"> </l-marker>
     </l-map>
   </div>
 </template>
@@ -52,6 +53,7 @@ export default {
     return {
       zoom: 13,
       center: latLng(35.319065, 139.550412),
+      currentCenter: latLng(35.319065, 139.550412),
       url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
       attribution:
         '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
@@ -61,85 +63,47 @@ export default {
       },
       withPopup: latLng(35.316715, 139.536041),
       withTooltip: latLng(35.319065, 139.550412),
-      //   currentZoom: 11.5,
-      //   currentCenter: latLng(47.41322, -1.219482),
-      showParagraph: false,
       mapOptions: {
         zoomSnap: 0.5,
       },
-      showMap: true,
-      currentLocation: latLng(0, 0),
-      watch: true,
     }
   },
   //   created() {
-  //     console.dir(LMap.options)
+
   //   },
   methods: {
-    //   初期状態で現在地表示
+    //   コンポーネントが読み込まれたら位置情報取得
     onReady(mapObject) {
       mapObject.locate()
-      //   console.log(this.$currentLocation)
     },
     onLocationFound(location) {
-      //   console.log(this.currentLocation)
-      this.currentLocation = location.latlng
+      // 取得した位置情報を中心にする
       this.center = location.latlng
-      //   console.log(location.latlng)
+      // 取得した位置情報にマーカーをおく
+      this.currentCenter = location.latlng
     },
-    // kamakura(){
-
-    // }
-    now() {
-      this.center = this.currentLocation
-      console.log('現在地取得した！')
-      // 緯度経度が全く同じ値だと地図中心が移動してくれないため、無理やり変更している。その後、現在地をセット
-      //   this.center = latLng(35.319065, 139.550412)
-      // Geolocation API で現在位置を取得
-      //   アロー関数にしないとthis使えない
-      //   navigator.geolocation.getCurrentPosition(
-      //     (position) => {
-      //       //   this.currentLocation = latLng(
-      //       //     position.coords.latitude,
-      //       //     position.coords.longitude
-      //       //   )
-
-      //       //   this.center = latLng(
-      //       //     position.coords.latitude,
-      //       //     position.coords.longitude
-      //       //   )
-
-      //       this.center = this.currentLocation
-      //       console.log('現在地取得した！')
-      //       //   console.log(position.coords.latitude, position.coords.longitude)
-      //       //   console.log(position.coords.accuracy)
-      //     },
-      //     (error) => {
-      //       alert(error.code)
-      //     },
-      //     { enableHighAccuracy: true }
-      //   )
-      //   this.center = this.currentLocation
-      //   console.log(this.currentLocation.lat)
-      //   console.dir(this.center)
+    // 下記二つ必須 ※これがないとzoomが更新されない
+    zoomUpdated(zoom) {
+      this.zoom = zoom
+    },
+    centerUpdated(center) {
+      this.center = center
+    },
+    kamakura() {
+      this.center = latLng(35.319065, 139.550412)
+    },
+    getCurrentPosition() {
+      this.center = this.currentCenter
     },
   },
+
   mounted() {
-    //   ページ開いたとき現在地を表示→methodsに記載
-    // navigator.geolocation.getCurrentPosition((position) => {
-    //   this.currentLocation = latLng(
-    //     position.coords.latitude,
-    //     position.coords.longitude
-    //   )
-    //   this.center = latLng(position.coords.latitude, position.coords.longitude)
-    // })
-    // 現在地を追跡→ちょっと難しい
+    // 現在地を追跡
     navigator.geolocation.watchPosition((position) => {
-      this.currentLocation = latLng(
+      this.currentCenter = latLng(
         position.coords.latitude,
         position.coords.longitude
       )
-      console.log('追跡した！')
     })
   },
 }
