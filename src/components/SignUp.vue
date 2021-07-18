92428ee0c0b3fd1f16ce68739de9e1725926ed98
DaikiOkada <d.okada0820@gmail.com>
2021-07-18 21:19:56 +0900

    メールアドレスとパスワードで、サインアップ、サインイン機能を実験

---
+++ src/components/SignUp.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <!-- 以下サインアップ機能 -->
+    <h1>サインアップ</h1>
+    <input type="email" placeholder="メールアドレス" v-model="email" />
+    <input type="password" placeholder="パスワード" v-model="password" />
+    <button @click="signUp">サインアップ</button>
+    <!-- 以下サインイン機能 -->
+    <h1>サインイン</h1>
+    <input type="email" placeholder="メールアドレス" v-model="email2" />
+    <input type="password" placeholder="パスワード" v-model="password2" />
+    <button @click="signIn">サインイン</button>
+  </div>
+</template>
+<script>
+import firebase from 'firebase'
+
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      email2: '',
+      password2: '',
+    }
+  },
+  methods: {
+    signUp() {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.email, this.password)
+        .then((userCredential) => {
+          // Signed in
+          var user = userCredential.user
+          console.log(user)
+          // ...
+        })
+    },
+    signIn() {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.email2, this.password2)
+        .then((userCredential) => {
+          // Signed in
+          var user = userCredential.user
+          console.log(user)
+          // ...
+        })
+    },
+  },
+}
+</script>
